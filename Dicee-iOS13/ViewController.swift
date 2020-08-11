51f67464ec4a05bd7413c761d1f72c24b39e3983
chajinjoo <chajanee@gmail.com>
2020-08-12 01:05:19 +0900

    첫커밋

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var diceImageView1: UIImageView!  // 첫번째 주사위
+    @IBOutlet var diceImageView2: UIImageView!  // 두번째 주사위
+    
+    var leftDiceNumber = 1
+    var rigthDiceNumber = 5
+    
+    override func viewDidLoad() {  // 뷰가 로드될 때 트리거 되는 코드라인
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
 
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][rigthDiceNumber]
+        
+        leftDiceNumber = leftDiceNumber + 1
+        rigthDiceNumber = rigthDiceNumber - 1
+
+    }
+    
 }
 
